29fa02e26f39750645e4b54c45782dcc130124a7
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-05-29 17:17:27 +0800

    Time: 99 ms (52.57%), Space: 15.1 MB (91.42%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -3,9 +3,10 @@ class Solution:
         l, r = 0, len(arr) - 1
         while l < r:
             mid = l + (r - l)//2
-            print(arr[mid])
+            # ascending
             if arr[mid] < arr[mid + 1]:
                 l = mid + 1
+            # descending
             else:
                 r = mid
         return l
