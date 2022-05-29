cb11cf7f4b86ed8251cedb6e4b224a229a4b7f5c
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-05-29 17:23:36 +0800

    Time: 71 ms (33.95%), Space: 14 MB (83.46%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -3,7 +3,6 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l < r:
             mid = l + (r - l)//2
-            print(nums[mid])
             if nums[mid] < nums[mid + 1]:
                 l = mid + 1
             else:
