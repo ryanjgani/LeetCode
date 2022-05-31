83bc0abecf8f6fc1e4f679b079a4f6b14bc1add8
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-05-31 23:05:30 +0800

    Time: 207 ms (8.56%), Space: 13.8 MB (93.32%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -5,20 +5,36 @@ class Solution:
         
         candidates.sort()
 
+        # Time: O(n*n^n)
+        # Space: O(k) where k = target
+        # def dfs(i):
+        #     if sum(comb) >= target or i >= len(candidates):
+        #         if sum(comb) == target:
+        #             res.append(comb[:])
+        #         return
+        #     skipvalue = 0
+        #     for j in range(i, len(candidates) - skipvalue):
+        #         idx = j + skipvalue
+        #         if idx == len(candidates):
+        #             break
+        #         comb.append(candidates[idx])
+        #         dfs(idx + 1)
+        #         comb.pop()
+        #         while j + skipvalue + 1 < len(candidates) and candidates[j + skipvalue] == candidates[j + skipvalue + 1]:
+        #             skipvalue += 1
+        
         def dfs(i):
             if sum(comb) >= target or i >= len(candidates):
                 if sum(comb) == target:
                     res.append(comb[:])
                 return
-            skipvalue = 0
-            for j in range(i, len(candidates) - skipvalue):
-                idx = j + skipvalue
-                if idx == len(candidates):
-                    break
-                comb.append(candidates[idx])
-                dfs(idx + 1)
+            for j in range(i, len(candidates)):
+                if j > i and candidates[j] == candidates[j - 1]:
+                    continue
+                comb.append(candidates[j])
+                dfs(j + 1)
                 comb.pop()
-                while j + skipvalue + 1 < len(candidates) and candidates[j + skipvalue] == candidates[j + skipvalue + 1]:
-                    skipvalue += 1
+        
+        
         dfs(0)
         return res
