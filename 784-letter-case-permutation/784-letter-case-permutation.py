20369a8a11c76628303f89569b411f7c6ee372d8
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-05-31 16:31:53 +0800

    Time: 82 ms (51.36%), Space: 15.5 MB (20.46%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,15 +1,32 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         # BFS solution - iterative
-        res = [s]
-        for i in range(len(s)):
-            temp = []
+        # res = [s]
+        # for i in range(len(s)):
+        #     temp = []
+        #     if s[i].isalpha():
+        #         while res:
+        #             string = res.pop(0)
+        #             left = string[:i]
+        #             right = string[i + 1:]
+        #             temp.append(left + s[i].upper() + right)
+        #             temp.append(left + s[i].lower() + right)
+        #         res = temp
+        # return res
+        
+        # DFS solution - recursive
+        res = []
+        
+        def dfs(sub, i):
+            if i >= len(s):
+                res.append(sub[:])
+                return
             if s[i].isalpha():
-                while res:
-                    string = res.pop(0)
-                    left = string[:i]
-                    right = string[i + 1:]
-                    temp.append(left + s[i].upper() + right)
-                    temp.append(left + s[i].lower() + right)
-                res = temp
+                # swap
+                dfs(sub + s[i].lower(), i + 1)
+                dfs(sub + s[i].upper(), i + 1)
+            else:
+                dfs(sub + s[i], i + 1)
+
+        dfs("", 0)
         return res
