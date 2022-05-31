df52bffe8662f192c5b569226f3ecd9993a58305
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-05-31 22:42:16 +0800

    Time: 217 ms (7.65%), Space: 14 MB (57.85%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -4,7 +4,7 @@ class Solution:
         comb = []
         
         candidates.sort()
-        print(candidates)
+
         def dfs(i):
             if sum(comb) >= target or i >= len(candidates):
                 if sum(comb) == target:
@@ -14,15 +14,10 @@ class Solution:
             for j in range(i, len(candidates) - skipvalue):
                 if j + skipvalue == len(candidates):
                     break
-                print(j + skipvalue, candidates[j + skipvalue])
                 comb.append(candidates[j + skipvalue])
-                print(comb)
                 dfs(j + skipvalue + 1)
                 comb.pop()
                 while j + skipvalue + 1 < len(candidates) and candidates[j + skipvalue] == candidates[j + skipvalue + 1]:
-                    print("here", j + skipvalue, i)
                     skipvalue += 1
-                    print("j becomes", j + skipvalue)
         dfs(0)
-        print("***", res)
         return res
