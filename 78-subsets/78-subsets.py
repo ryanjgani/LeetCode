f3152e66f2643af5ecc54ce9d8b79f5e22a105f4
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-05-31 17:44:27 +0800

    Time: 33 ms (91.73%), Space: 14.1 MB (81.00%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -3,15 +3,12 @@ class Solution:
         res = []
         subset = []
         def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+            if i == len(nums):
+                res.append(subset[:])
                 return
-            
             subset.append(nums[i])
             dfs(i + 1)
             subset.pop()
             dfs(i + 1)
         dfs(0)
         return res
-            
-            
