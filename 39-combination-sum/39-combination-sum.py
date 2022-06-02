4efdbec7d39333bdcd7afa6d18cafa71053ee0c9
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-02 13:07:29 +0800

    Time: 172 ms (20.59%), Space: 14.1 MB (25.92%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -4,17 +4,24 @@ class Solution:
         comb = []
         candidates.sort()
         
-        # def dfs(i, remaining):
-        #     if remaining == 0:
-        #         res.append(comb[:])
-        #         return
-        #     if remaining < 0:
-        #         return # backtrack
+        def dfs(i, remaining):
+            if remaining == 0:
+                res.append(comb[:])
+                return
+            if i >= len(candidates) or remaining < 0:
+                return # backtrack
             
+            comb.append(candidates[i])
+            dfs(i, remaining - candidates[i])
+            comb.pop()
+            dfs(i + 1, remaining)
+        
+    
+    
     
         # Time: O(n^k) where k = target
         # Space: O(k) -> max space of comb, not counting the output array res
-        def dfs(idx):
+        def dfs_long(idx):
             if sum(comb) == target:
                 res.append(comb[:])
                 return
@@ -27,5 +34,5 @@ class Solution:
                 comb.append(candidates[i])
                 dfs(i)
                 comb.pop()
-        dfs(0)
+        dfs(0, target)
         return res
