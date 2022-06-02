776289fb83f634461fecd19091450698e732b08e
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-02 13:20:03 +0800

    Time: 226 ms (12.55%), Space: 14.2 MB (9.09%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -4,6 +4,8 @@ class Solution:
         comb = []
         candidates.sort()
         
+        # Time: O(2^k) where k = target
+        # Because for each branch, we either include the element or don't.
         def dfs(i, remaining):
             if remaining == 0:
                 res.append(comb[:])
@@ -15,11 +17,8 @@ class Solution:
             dfs(i, remaining - candidates[i])
             comb.pop()
             dfs(i + 1, remaining)
-        
     
-    
-    
-        # Time: O(n^k) where k = target
+        # Time: O(n^k) where k = target but with lots of pruning
         # Space: O(k) -> max space of comb, not counting the output array res
         def dfs_long(idx):
             if sum(comb) == target:
@@ -34,5 +33,6 @@ class Solution:
                 comb.append(candidates[i])
                 dfs(i)
                 comb.pop()
+                
         dfs(0, target)
         return res
