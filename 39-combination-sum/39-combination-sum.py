0d29f84e34ba47098bc8241b2dcbf17eef802944
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-06 22:42:40 +0800

    Time: 130 ms (40.25%), Space: 14.2 MB (9.38%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,37 +2,14 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         comb = []
-        candidates.sort()
-        
-        # Time: O(2^k) where k = target
-        # Because for each branch, we either include the element or don't.
-        def dfs(i, remaining):
-            if remaining == 0:
+        def dfs(idx, rem):
+            if rem == 0:
                 res.append(comb[:])
+            if rem < 0:
                 return
-            if i >= len(candidates) or remaining < 0:
-                return # backtrack
-            
-            comb.append(candidates[i])
-            dfs(i, remaining - candidates[i])
-            comb.pop()
-            dfs(i + 1, remaining)
-    
-        # Time: O(n^k) where k = target but with lots of pruning
-        # Space: O(k) -> max space of comb, not counting the output array res
-        def dfs_long(idx):
-            if sum(comb) == target:
-                res.append(comb[:])
-                return
-            if sum(comb) > target:
-                return # backtrack
-
             for i in range(idx, len(candidates)):
-                if sum(comb) + candidates[i] > target:
-                    break
                 comb.append(candidates[i])
-                dfs(i)
+                dfs(i, rem - candidates[i])
                 comb.pop()
-                
         dfs(0, target)
         return res
