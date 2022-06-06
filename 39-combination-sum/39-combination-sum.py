db4e93ef722850f98ba0a3dcfb8e10da04207688
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-06 22:46:42 +0800

    Time: 98 ms (65.81%), Space: 14.1 MB (12.49%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,12 +2,15 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         comb = []
+        candidates.sort()
         def dfs(idx, rem):
             if rem == 0:
                 res.append(comb[:])
             if rem < 0:
                 return
             for i in range(idx, len(candidates)):
+                if rem - candidates[i] < 0:
+                    break
                 comb.append(candidates[i])
                 dfs(i, rem - candidates[i])
                 comb.pop()
