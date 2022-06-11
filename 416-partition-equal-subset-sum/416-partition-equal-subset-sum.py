4394a11f71b7752b62c515931b73e7394df8537c
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-11 17:06:51 +0800

    Time: 312 ms (86.49%), Space: 28.4 MB (50.52%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,22 +1,17 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if not nums:
-            return True
-        n = len(nums)
-        if sum(nums) % 2 != 0:
-            return False
-        target = sum(nums)//2
-        memo = {}
-        
-        def helper(total, i):
-            nonlocal nums, memo
-            if (total, i) in memo:
-                return memo[(total, i)]
-            if i == len(nums):
-                return False
-            if total == 0:
+        dp = {}
+        def dfs(i, rem):
+            if (i, rem) in dp:
+                return dp[(i, rem)]
+            
+            if rem == 0:
                 return True
-            memo[(total, i)] = helper(total-nums[i], i+1) or helper(total, i+1)
-            return memo[(total, i)]
+            if rem < 0 or i >= len(nums):
+                return False
+
+            dp[(i, rem)] = dfs(i + 1, rem - nums[i]) or dfs(i + 1, rem)
+            return dp[(i, rem)]
         
-        return helper(target, 0) 
+        target = sum(nums) / 2
+        return False if sum(nums) % 2 else dfs(0, target)
