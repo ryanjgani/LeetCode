6b73f96bd2373374a2d478b9399bff8a0fccfbeb
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-11 17:00:27 +0800

    Time: 2968 ms (26.59%), Space: 31.9 MB (20.40%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,32 +1,16 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # Bottom up
-        if sum(nums) % 2: return False
-        dp = set()
-        dp.add(0)
-        target = sum(nums) / 2
-        
-        for i in range(len(nums) - 1, -1, -1):
-            temp = set()
-            for n in dp:
-                temp.add(n + nums[i])
-                temp.add(n)
-            dp = temp
-        return True if target in dp else False
-        
-        
-        # Brute force O(2^n)
-        # target = sum(nums) // 2
-        # if target != sum(nums) / 2: return False
-        # comb = []
-        # def dfs(i):
-        #     if sum(comb) == target:
-        #         return True
-        #     if sum(comb) > target or i >= len(nums):
-        #         return False
-        #     comb.append(nums[i])
-        #     add = dfs(i + 1)
-        #     comb.pop()
-        #     not_add = dfs(i + 1)
-        #     return add or not_add
-        # return dfs(0)
+        cache = {}
+        def helper(start, target):         # Here path is not needed
+            if (start, target) in cache:
+                return cache[(start, target)]
+            if target < 0:
+                return
+            elif target == 0:
+                return True
+            for i in range(start, len(nums)):
+                if helper(i+1, target-nums[i]):
+                    return True
+            cache[(start, target)] = False
+            return False
+        return False if sum(nums)%2 else helper(0, sum(nums)/2)
