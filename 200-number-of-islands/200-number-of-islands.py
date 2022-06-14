2f685290237eb25751a6b2655152bb25dd07ba58
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-14 22:42:53 +0800

    Time: 407 ms (58.78%), Space: 16.2 MB (90.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,10 +4,13 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         res = 0
         
+        # def dfs(r, c):
+        #     if r not in range(rows) or c not in range(cols) or grid[r][c] != 1
+        
         def bfs(r, c):
             q = []
             q.append((r, c))
-            visit.add((r, c))
+            grid[r][c] = '#'
             while q:
                 row, col = q.pop(0)
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
@@ -15,13 +18,13 @@ class Solution:
                 for dr, dc in directions:
                     r = dr + row
                     c = dc + col
-                    if r in range(rows) and c in range(cols) and grid[r][c] == '1' and (r, c) not in visit:
-                        visit.add((r, c))
+                    if r in range(rows) and c in range(cols) and grid[r][c] == '1':
+                        grid[r][c] = '#'
                         q.append((r, c))
         
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in visit:
+                if grid[r][c] == '1':
                     bfs(r, c)
                     res += 1
                 
