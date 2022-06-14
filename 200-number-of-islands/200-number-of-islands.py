f502af0c3c0dc48a463f208bd3f2dfe9ba0434b0
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-14 22:52:10 +0800

    Time: 835 ms (5.01%), Space: 16.4 MB (63.43%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,31 +1,37 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visit = set()
         rows, cols = len(grid), len(grid[0])
         res = 0
         
-        # def dfs(r, c):
-        #     if r not in range(rows) or c not in range(cols) or grid[r][c] != 1
-        
-        def bfs(r, c):
-            q = []
-            q.append((r, c))
+        def dfs(r, c):
+            if r not in range(rows) or c not in range(cols) or grid[r][c] != '1':
+                return
             grid[r][c] = '#'
-            while q:
-                row, col = q.pop(0)
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        
+#         def bfs(r, c):
+#             q = []
+#             q.append((r, c))
+#             grid[r][c] = '#'
+#             while q:
+#                 row, col = q.pop(0)
+#                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
                 
-                for dr, dc in directions:
-                    r = dr + row
-                    c = dc + col
-                    if r in range(rows) and c in range(cols) and grid[r][c] == '1':
-                        grid[r][c] = '#'
-                        q.append((r, c))
+#                 for dr, dc in directions:
+#                     r = dr + row
+#                     c = dc + col
+#                     if r in range(rows) and c in range(cols) and grid[r][c] == '1':
+#                         grid[r][c] = '#'
+#                         q.append((r, c))
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
-                    bfs(r, c)
+                    dfs(r, c)
                     res += 1
                 
         return res
