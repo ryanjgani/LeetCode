786746a47f22a7dda592131e12ec1283187e33c3
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-23 22:43:00 +0800

    Time: 45 ms (57.10%), Space: 13.8 MB (70.61%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
+        # if len(nums) == 1: return nums[0]
         def helper(nums):
             rob1, rob2 = 0, 0
             for n in nums:
@@ -9,4 +9,4 @@ class Solution:
                 rob2 = tmp
             return rob2
         
-        return max(helper(nums[1:]), helper(nums[:-1]))
+        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
