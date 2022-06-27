2fbfcb86bb884c00315c6da6210cb3741f822a50
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-27 23:23:04 +0800

    Time: 1732 ms (12.31%), Space: 15.4 MB (6.88%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,42 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # Greedy O(n)
-        goalpost = len(nums) - 1
+        target = len(nums) - 1
+        if len(nums) == 1: return True
+        
         for i in range(len(nums) - 2, -1, -1):
-            if nums[i] >= goalpost - i:
-                goalpost = i
-        return True if goalpost == 0 else False
-        
-        # top down memoization O(n^2) - TLE
-        cache = {}
-        def cache_dfs(idx):
-            if idx in cache:
-                return cache[idx]
-            if idx == len(nums) - 1:
-                return True
-            if idx >= len(nums):
-                return False
-            
-            for i in range(1, nums[idx] + 1):
-                if cache_dfs(idx + i):
-                    cache[idx] = True
-                    return True
-            cache[idx] = False
-            return False
-        return cache_dfs(0)
-        
-        
-        
-        
-        # Brute force approach O(n ^ n) - TLE
-        def brute_dfs(idx):
-            if idx == len(nums) - 1:
-                return True
-            if idx >= len(nums):
-                return False
-            
-            for i in range(1, nums[idx] + 1):
-                if brute_dfs(idx + i):
-                    return True
-            return False
-        return brute_dfs(0)
+            print(nums[i], i, target)
+            if nums[i] >= target - i:
+                target = i
+        return target == 0
