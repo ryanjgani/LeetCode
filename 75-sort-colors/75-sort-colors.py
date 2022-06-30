8f8c0f58d78c2458c2ac64a94f2c3f870680a4b9
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-30 22:46:28 +0800

    Time: 29 ms (98.19%), Space: 13.9 MB (15.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,18 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # l, r = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1
+        i = 0
+        while i < len(nums):
+            if i > r:
+                break
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+                if nums[i] == 0 or nums[i] == 2:
+                    i -= 1
+            i += 1
+            
+                
         
         # Bucket Sort
-        bucket = [0] * 3
-        for n in nums:
-            bucket[n] += 1
-        idx = 0
-        for i in range(len(nums)):
-            while bucket[idx] == 0:
-                idx += 1
-            nums[i] = idx
-            bucket[idx] -= 1
+        # bucket = [0] * 3
+        # for n in nums:
+        #     bucket[n] += 1
+        # idx = 0
+        # for i in range(len(nums)):
+        #     while bucket[idx] == 0:
+        #         idx += 1
+        #     nums[i] = idx
+        #     bucket[idx] -= 1
             
         
         
