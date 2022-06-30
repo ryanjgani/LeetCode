6100f70b5f1e91d8a5fbcb0da036fcff89cb00af
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-06-30 22:49:39 +0800

    Time: 53 ms (44.50%), Space: 13.8 MB (63.37%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -5,17 +5,14 @@ class Solution:
         """
         l, r = 0, len(nums) - 1
         i = 0
-        while i < len(nums):
-            if i > r:
-                break
+        while i <= r:
             if nums[i] == 0:
                 nums[i], nums[l] = nums[l], nums[i]
                 l += 1
             elif nums[i] == 2:
                 nums[i], nums[r] = nums[r], nums[i]
                 r -= 1
-                if nums[i] == 0 or nums[i] == 2:
-                    i -= 1
+                i -= 1
             i += 1
             
                 
