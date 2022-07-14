70f2136dc22c9c411471f599acac1ee88e4974f6
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-14 15:55:50 +0800

    Time: 3813 ms (9.63%), Space: 30.4 MB (28.31%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -4,15 +4,11 @@ class Solution:
         l = 0
         deque = []
         for r in range(len(nums)):
-            if r < k - 1:
-                while deque and deque[-1] < nums[r]:
-                    deque.pop()
-                deque.append(nums[r])
-            else:
-                while deque and deque[-1] < nums[r]:
-                    deque.pop()
-                deque.append(nums[r])
-                
+
+            while deque and deque[-1] < nums[r]:
+                deque.pop()
+            deque.append(nums[r])
+            if r + 1 >= k:
                 res.append(deque[0])
                 if nums[l] == deque[0]:
                     deque.pop(0)
