01ace30dcf51f24abd25d429e23568381cdb8269
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-15 15:35:13 +0800

    Time: 233 ms (5.10%), Space: 14 MB (6.28%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        for row in board:
+            temp = set()
+            for n in row:
+                if n == '.': continue
+                if int(n) not in temp:
+                    temp.add(int(n))
+                else:
+                    return False
+                    
+        for c in range(len(board[0])):
+            temp = set()
+            for r in range(len(board)):
+                if board[r][c] == '.': continue
+                n = int(board[r][c])
+                if n not in temp:
+                    temp.add(n)
+                else:
+                    return False
+                
+        hmap = {}
+        for i in range(3):
+            for j in range(3):
+                hmap[(i, j)] = set()
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == '.': continue
+                key = (r // 3, c // 3)
+                number = int(board[r][c])
+                if number in hmap[key]:
+                    return False
+                else:
+                    hmap[key].add(number)
+        return True
+                
