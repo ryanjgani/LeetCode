a68e9d1998094d04fc4f15574d227f229ba129aa
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-16 17:23:29 +0800

    Time: 2907 ms (19.17%), Space: 44.6 MB (5.50%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,15 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {}
+        dp = {}       
         res = float('inf')
         
         def dfs(rem):
             if rem in dp: return dp[rem]
-            if rem == 0: return 0
-            if rem < 0: return float('inf')
-            
-            dp[rem] = min([1 + dfs(rem - c) for c in coins])
+            if rem == 0:
+                return 0
+            if rem < 0:
+                return float('inf')
+            tmp = float('inf')
+            for c in coins:
+                tmp = min(tmp, 1 + dfs(rem - c))
+            dp[rem] = tmp
             return dp[rem]
-        
+            
         res = dfs(amount)
         return res if res < float('inf') else -1
