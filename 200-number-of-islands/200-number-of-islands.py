8e8fc077982d9f862977df1b9759d22ed4b0a218
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-16 20:04:06 +0800

    Time: 549 ms (33.24%), Space: 16.2 MB (97.39%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -5,18 +5,35 @@ class Solution:
         res = 0
         visited = [([False] * COLS) for _ in range(ROWS)]
         
-        def dfs(r, c):
-            if r >= ROWS or c >= COLS or c < 0 or r < 0 or visited[r][c] or grid[r][c] == '0':
-                return
-            visited[r][c] = True
-            dfs(r, c + 1)
-            dfs(r + 1, c)
-            dfs(r, c - 1)
-            dfs(r - 1, c)
-        
+        stack = []
         for i in range(ROWS):
             for j in range(COLS):
                 if not visited[i][j] and grid[i][j] == '1':
-                    dfs(i, j)
+                    queue = [[i, j]]
+                    while queue:
+                        r, c = queue.pop(0)
+                        if r >= ROWS or c >= COLS or c < 0 or r < 0 or visited[r][c] or grid[r][c] == '0': continue
+                        visited[r][c] = True
+                        queue.append([r, c + 1])
+                        queue.append([r + 1, c])
+                        queue.append([r, c - 1])
+                        queue.append([r - 1, c])
                     res += 1
         return res
+        
+        
+#         def dfs(r, c):
+#             if r >= ROWS or c >= COLS or c < 0 or r < 0 or visited[r][c] or grid[r][c] == '0':
+#                 return
+#             visited[r][c] = True
+#             dfs(r, c + 1)
+#             dfs(r + 1, c)
+#             dfs(r, c - 1)
+#             dfs(r - 1, c)
+        
+#         for i in range(ROWS):
+#             for j in range(COLS):
+#                 if not visited[i][j] and grid[i][j] == '1':
+#                     dfs(i, j)
+#                     res += 1
+#         return res
