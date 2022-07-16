ce90b663edf2083b5cc525d802022c9d1d5910fc
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-16 17:21:53 +0800

    Time: 2674 ms (26.39%), Space: 38.4 MB (10.04%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,32 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # bottom up
-        maxNum = float('inf')
         dp = {}
-        dp[0] = 0
+        res = float('inf')
         
-        for a in range(1, amount + 1):
-            dp[a] = maxNum
-            temp = dp[a]
-            for c in coins:
-                if a - c >= 0:
-                    temp = min(1 + dp[a - c], temp)
-            dp[a] = temp
-        
-        return dp[amount] if dp[amount] < maxNum else -1
-        
-        
-        # top down
-#         dp = {}
-        
-#         def dfs(rem):
-#             if rem in dp: return dp[rem]
-#             if rem == 0: return 0
-#             if rem < 0: return float('inf')
+        def dfs(rem):
+            if rem in dp: return dp[rem]
+            if rem == 0: return 0
+            if rem < 0: return float('inf')
             
-#             dp[rem] = min([1 + dfs(rem - c) for c in coins])
-#             return dp[rem]
+            dp[rem] = min([1 + dfs(rem - c) for c in coins])
+            return dp[rem]
         
-#         res = dfs(amount)
-#         return res if res < float('inf') else -1
-        
+        res = dfs(amount)
+        return res if res < float('inf') else -1
