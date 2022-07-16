0c3379c72e0467b2279e6515c1aea7373e2ed2a9
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-16 19:39:14 +0800

    Time: 801 ms (25.21%), Space: 27.2 MB (63.89%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,35 +1,13 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        l, r = 0, len(cardPoints) - k
-        total = sum(cardPoints[r:])
-        res = total
-        
-        while r < len(cardPoints):
-            total += cardPoints[l] - cardPoints[r]
-            res = max(res, total)
-            r += 1
-            l += 1
+        wlen = len(cardPoints) - k
+        curSum = res = l = 0
+        total = sum(cardPoints)
+        for i in range(len(cardPoints)):
+            if i >= wlen:
+                res = max(total - curSum, res)
+                curSum -= cardPoints[l]
+                l += 1
+            curSum += cardPoints[i]
+        res = max(total - curSum, res)
         return res
-        
-        
-        # brute force O(2^n)
-#         res = 0
-#         dp = {}
-        
-#         def dfs(count, cards, curRes):
-#             nonlocal res
-#             if count == k:
-#                 res = max(res, curRes)
-#                 return
-#             # left
-#             left = cards.pop(0)
-#             dfs(count + 1, cards[:], curRes + left)         
-#             cards = [left] + cards  
-            
-#             # right
-#             right = cards.pop()
-#             dfs(count + 1, cards[:], curRes + right)
-#             cards.append(right)
-            
-#         dfs(0, cardPoints, 0)
-#         return res
