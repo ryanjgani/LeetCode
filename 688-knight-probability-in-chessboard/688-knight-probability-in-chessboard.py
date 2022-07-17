68dc233ca89fafae05eed4d32f6628e0f2300c46
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-17 15:01:17 +0800

    Time: 261 ms (91.66%), Space: 23.7 MB (20.05%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
@@ -15,22 +15,66 @@ class Solution:
 #                     position.append((r + i, c + j, step + 1, prob / 8))
 #         return res
         # graph solution O(8^k) time and space (TLE)
-        # DP solution
+       
+        # DP solution O(n^2 * k) time and O(n^2) space
         
-        dp = [[0] * n for _ in range(n)]
-        dp[row][column] = 1
-        for p in range(k):
-            temp = [[0] * n for _ in range(n)]
-            for r in range(n):
-                for c in range(n):
+        # dp = [[0] * n for _ in range(n)]
+        # dp[row][column] = 1
+        # for p in range(k):
+        #     temp = [[0] * n for _ in range(n)]
+        #     for r in range(n):
+        #         for c in range(n):
+        #             for i, j in moves:
+        #                 if r + i < n and c + j < n and r + i >= 0 and c + j >= 0:
+        #                     temp[r + i][c + j] += dp[r][c] / 8
+        #     dp = temp
+        # res = 0
+        # for row in dp:
+        #     res += sum(row)
+        # return res
+        
+        mem = {}
+        def dfs(r, c, prob, step):
+            p = 0
+            if 0 <= r < n and 0 <= c < n:
+                if step < k:
                     for i, j in moves:
-                        if r + i < n and c + j < n and r + i >= 0 and c + j >= 0:
-                            temp[r + i][c + j] += dp[r][c] / 8
-            dp = temp
-        res = 0
-        for row in dp:
-            res += sum(row)
-        return res
+                        nextR, nextC = r + i, c + j
+                        if (nextR, nextC, step + 1) not in mem:
+                            mem[(nextR, nextC, step + 1)] = dfs(nextR, nextC, prob / 8, step + 1)
+                        p += mem[(nextR, nextC, step + 1)] 
+                else:
+                    p = prob
+            return p
+        return dfs(row, column, 1, 0)    
+    
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
                     
                     
                     
