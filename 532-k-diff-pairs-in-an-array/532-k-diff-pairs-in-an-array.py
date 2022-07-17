6a2ec810c15a80b6b6655095f03054960b35b318
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-17 16:07:35 +0800

    Time: 138 ms (38.11%), Space: 15.7 MB (40.73%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -4,10 +4,8 @@ class Solution:
         res = 0
         if k == 0:
             for c in hmap:
-                if hmap[c] > 1:
-                    res += 1
-            return res
-        for key, val in hmap.items():
-            if key + k in hmap:
-                res += 1
+                res += 1 if hmap[c] > 1 else 0
+        else:
+            for key, val in hmap.items():
+                res += 1 if key + k in hmap else 0
         return res
