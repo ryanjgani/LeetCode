3ed3f063bd7cbb06b8db8420a58666d7ce88ca2f
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-18 22:46:02 +0800

    Time: 3868 ms (5.00%), Space: 37.4 MB (6.43%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,24 +1,19 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        # O(n^2) worst but O(n) average time, Quick Select method (using quick sort)
         target = len(nums) - k
-        count = 0
-        def quicksort(start, p):
-            partition = nums[p]
-            l, r = start, p - 1
-            for i in range(start, p):
+        def quicksort(start, end):
+            partition = nums[end]
+            l = start
+            for i in range(start, end):
                 if nums[i] < partition:
                     nums[i], nums[l] = nums[l], nums[i]
                     l += 1
-            nums[p], nums[l] = nums[l], nums[p]
+            nums[end], nums[l] = nums[l], nums[end]
             if l == target:
                 return nums[l]
-            return quicksort(l + 1, p) if target > l else quicksort(start, l - 1)
+            return quicksort(l + 1, end) if target > l else quicksort(start, l - 1)
         return quicksort(0, len(nums) - 1)
-        
-        
-        
-        
-        
         
         # O(n + klogn) time and O(n) space
         # heap = []
