3e749b7cb75a57761b830b77c0814564b448f438
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-18 15:48:15 +0800

    Time: 352 ms (51.78%), Space: 25.3 MB (75.11%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,20 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # res = nums[:]
-        # for i in range(len(nums)):
-        #     res[(i + k) % len(nums)] = nums[i]
-        # print(res)
-        k = k % len(nums)
-        nums.reverse()
-        l, r = 0, k - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-        l, r = k, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+        res = nums[:]
+        for i in range(len(nums)):
+            res[(i + k) % len(nums)] = nums[i]
+        for i in range(len(nums)):
+            nums[i] = res[i]
+        
+        # k = k % len(nums)
+        # nums.reverse()
+        # l, r = 0, k - 1
+        # while l < r:
+        #     nums[l], nums[r] = nums[r], nums[l]
+        #     l += 1
+        #     r -= 1
+        # l, r = k, len(nums) - 1
+        # while l < r:
+        #     nums[l], nums[r] = nums[r], nums[l]
+        #     l += 1
+        #     r -= 1
             
