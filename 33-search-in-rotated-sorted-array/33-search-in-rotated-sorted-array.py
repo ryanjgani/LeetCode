9dbced8581ff6bf73292b99acc64ecffb6921825
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-18 17:13:55 +0800

    Time: 69 ms (40.44%), Space: 14.2 MB (90.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,33 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
-        
-        while l < r:
+        while l <= r:
             mid = l + (r - l) // 2
-            if nums[mid] >= nums[l] and nums[mid] >= nums[r]:
-                l = mid + 1
+            if nums[mid] == target: return mid
+            
+            if nums[mid] >= nums[l]:
+                if target > nums[mid]: l = mid + 1
+                else:
+                    if target < nums[l]: l = mid + 1
+                    else: r = mid - 1
             else:
-                r = mid
-        l1, r1 = 0, l - 1
-        l2, r2 = l, len(nums) - 1
-        print(l)
-        while l1 <= r1:
-            mid = l1 + (r1 - l1) // 2
-            if target > nums[mid]:
-                l1 = mid + 1
-            elif target < nums[mid]:
-                r1 = mid - 1
-            else:
-                return mid
-
-        while l2 <= r2:
-            mid = l2 + (r2 - l2) // 2
-            if target > nums[mid]:
-                l2 = mid + 1
-            elif target < nums[mid]:
-                r2 = mid - 1
-            else:
-                return mid
-        return -1
-        
-                
+                if target < nums[mid]: r = mid - 1
+                else:
+                    if target <= nums[r]: l = mid + 1
+                    else: r = mid - 1
+        return - 1
+            
+            # if mid on the left portion and target > mid: l = mid + 1
+            # if mid on the left portion and target < mid: target can be on left or right
+            
+            # if mid on right and target < mid: r = mid - 1
+            # if mid on right and target > mid: target can be on left or right
+            #   if target <= r: l = mid + 1
+            #   else: r = mid - 1
+            
