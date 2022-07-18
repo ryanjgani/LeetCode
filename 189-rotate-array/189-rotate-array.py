0a164c4b41981931c591d60f034e68bd3b33da91
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-18 15:49:02 +0800

    Time: 341 ms (55.07%), Space: 25.3 MB (96.76%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,22 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        res = nums[:]
-        for i in range(len(nums)):
-            res[(i + k) % len(nums)] = nums[i]
-        for i in range(len(nums)):
-            nums[i] = res[i]
+        # res = nums[:]
+        # for i in range(len(nums)):
+        #     res[(i + k) % len(nums)] = nums[i]
+        # for i in range(len(nums)):
+        #     nums[i] = res[i]
         
-        # k = k % len(nums)
-        # nums.reverse()
-        # l, r = 0, k - 1
-        # while l < r:
-        #     nums[l], nums[r] = nums[r], nums[l]
-        #     l += 1
-        #     r -= 1
-        # l, r = k, len(nums) - 1
-        # while l < r:
-        #     nums[l], nums[r] = nums[r], nums[l]
-        #     l += 1
-        #     r -= 1
+        # linear time and constant space
+        k = k % len(nums)
+        nums.reverse()
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
             
