4456d16f127e053d93d9f021da20818ff9c32ac4
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-19 22:30:52 +0800

    Time: 65 ms (13.48%), Space: 14.2 MB (11.03%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,31 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # brute force
+        res = 0
+        memo = {}
+        memo[(m - 1, n - 1)] = 1
+        def dfs(i, j):
+            nonlocal res
+            if (i, j) in memo: return memo[(i, j)]
+            if i >= m or j >= n:
+                
+                return 0
+            memo[(i, j)] = dfs(i, j + 1) + dfs(i + 1, j)
+            return memo[(i, j)] 
+        return dfs(0, 0)
         
-        row = [1] * n
-        
-        for i in range(m - 1):
-            newRow = [1] * n
-            for j in range(n - 2, -1, -1):
-                newRow[j] = row[j] + newRow[j + 1]
-            row = newRow
-        return row[0]
-        
-        
-        
-        # Top down memoization
-        # TC: O(n * m)
-        # SC: O(n * m)
-        
-        # count = 0
-        # cache = {}
-        # def dfs(i, j):
-        #     if i == m - 1 and j == n - 1:
-        #         return 1
-        #     if i >= m or j >= n:
-        #         return 0
-        #     if (i, j) in cache:
-        #         return cache[(i, j)]
-        #     # go right and go down
-        #     cache[(i, j)] = dfs(i, j + 1) + dfs(i + 1, j)
-        #     return cache[(i, j)]
-        # return dfs(0, 0)
