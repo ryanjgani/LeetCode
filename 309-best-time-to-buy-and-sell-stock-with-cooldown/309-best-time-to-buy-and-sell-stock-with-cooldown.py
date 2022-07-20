07d8bdf3cf411276f42bf53d48f0a2630788dc3b
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-20 22:14:36 +0800

    Time: 75 ms (41.53%), Space: 17.8 MB (31.17%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         res = 0
-        holding = False
+        buying = True
         dp = {}
         
         def dfs(i, buying):
-            if i >= len(prices): return 0
             if (i, buying) in dp: return dp[(i, buying)]
-            
+            if i >= len(prices):
+                return 0
             if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
+                buy = dfs(i + 1, False) - prices[i]
                 cooldown = dfs(i + 1, buying)
                 dp[(i, buying)] = max(buy, cooldown)
             else:
-                sell = dfs(i + 2, not buying) + prices[i]
+                sell = dfs(i + 2, True) + prices[i]
                 cooldown = dfs(i + 1, buying)
                 dp[(i, buying)] = max(sell, cooldown)
             return dp[(i, buying)]
