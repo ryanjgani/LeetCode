5bf125d1e001d09b5456cd83d623fd110ae16f09
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-21 22:02:52 +0800

    Time: 91 ms (99.98%), Space: 15.3 MB (54.33%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -2,15 +2,30 @@ class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         prices = [float('inf')] * n
         prices[src] = 0
-        queue = [flights[0]]
+        dp = {}
+        for s, d, p in flights:
+            if s in dp:
+                dp[s].append((d, p))
+            else:
+                dp[s] = [(d, p)]
+                
+        queue = [(src, 0, -1)]
         
-        while k + 1:
-            n = len(queue)
-            temp = prices[:]
-            for s, d, p in flights:
-                temp[d] = min(temp[d], prices[s] + p)
-            prices = temp
-            k -= 1
+        while queue:
+            pos, cost, step = queue.pop(0)
+            if pos == dst or step == k or pos not in dp: continue
+            for dest, price in dp[pos]:
+                if cost + price < prices[dest]:
+                    prices[dest] = cost + price
+                    queue.append((dest, cost + price, step + 1))
+
+        # while k + 1:
+        #     n = len(queue)
+        #     temp = prices[:]
+        #     for s, d, p in flights:
+        #         temp[d] = min(temp[d], prices[s] + p)
+        #     prices = temp
+        #     k -= 1
         return prices[dst] if prices[dst] < float('inf') else -1
         
         
