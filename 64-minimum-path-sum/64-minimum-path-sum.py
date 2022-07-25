fa9b32819914e02fd8390fc2f8133cbb35f71f4d
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-25 23:44:54 +0800

    Time: 207 ms (17.41%), Space: 15.8 MB (54.01%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -9,18 +9,17 @@ class Solution:
                 dp[r][c] = min(dp[r][c + 1], dp[r + 1][c]) + grid[r][c]
         return dp[0][0]
     
-#           top down memoization
-#         memo = {}
-#         def dfs(i, j):
-#             if (i, j) in memo: 
-#                 return memo[(i, j)]
-#             if i == ROWS - 1 and j == COLS - 1:
-#                 return grid[i][j]
-#             if i >= ROWS or j >= COLS:
-#                 return float('inf')
-#             left = dfs(i + 1, j) + grid[i][j]
-#             down = dfs(i, j + 1) + grid[i][j]
-#             memo[(i, j)] = min(left, down)
-#             return memo[(i, j)]
-#         res = dfs(0, 0)
-#         return res
+        # top down memoization
+        memo = {}
+        def dfs(i, j):
+            if (i, j) in memo: 
+                return memo[(i, j)]
+            if i == ROWS - 1 and j == COLS - 1:
+                return grid[i][j]
+            if i >= ROWS or j >= COLS:
+                return float('inf')
+            left = dfs(i + 1, j) + grid[i][j]
+            down = dfs(i, j + 1) + grid[i][j]
+            memo[(i, j)] = min(left, down)
+            return memo[(i, j)]
+        return dfs(0, 0)
