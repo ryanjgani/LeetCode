31d7ea99a39d1f78ad18d48d6bd15d76f0cdd131
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-25 23:24:00 +0800

    Time: 48 ms (56.47%), Space: 14.2 MB (10.75%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,20 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # res = 0
         alphabets = "&ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         alpha = {letter: idx for idx, letter in enumerate(alphabets)}
         memo = {len(s): 1}
         
-        def dfs(idx, comb):
-            nonlocal res
-            if idx in memo:
-                return memo[idx]
-            count = 0
-            for i in range(idx + 1, len(s) + 1):
-                if s[idx: i][0] != '0' and int(s[idx: i]) <= 26:
-                    count += dfs(i, comb + [s[idx: i]])
-            memo[idx] = count
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            # if i == len(s):
+            #     return 1
+            if s[i] == '0':
+                return 0
+            count = dfs(i + 1)
+            if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+                count += dfs(i + 2)
+            memo[i] = count
             return count
-                
-        res = dfs(0, [])
+            
+        res = dfs(0)
         return res
