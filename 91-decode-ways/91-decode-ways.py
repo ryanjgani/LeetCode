0ddb9ac402b579fbd952a099d1e34937edb70c17
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-25 23:35:01 +0800

    Time: 63 ms (21.20%), Space: 13.8 MB (80.49%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,16 +1,33 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # Bottom up
+        
         dp = [0] * (len(s) + 1)
-        dp[len(s)] = 1
+        prev = doubleprev = 1
         for i in range(len(s) - 1, -1, -1):
             if s[i] == '0':
-                dp[i] = 0
-                continue
-            dp[i] = dp[i + 1]
-            if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
-                dp[i] += dp[i + 2]
-        return dp[0]        
+                current = 0
+            else:
+                current = prev
+                if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+                    current += doubleprev
+            doubleprev = prev
+            prev = current
+            
+        return current
+        
+        
+        
+        # Bottom up O(n) time and space
+        # dp = [0] * (len(s) + 1)
+        # dp[len(s)] = 1
+        # for i in range(len(s) - 1, -1, -1):
+        #     if s[i] == '0':
+        #         dp[i] = 0
+        #         continue
+        #     dp[i] = dp[i + 1]
+        #     if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+        #         dp[i] += dp[i + 2]
+        # return dp[0]        
         
         
 #           Top Down Memo O(n) Time        
