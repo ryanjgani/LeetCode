0a5da8c513e765d564d7d8973abbac11af66e34c
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-25 23:31:19 +0800

    Time: 56 ms (36.27%), Space: 13.9 MB (80.49%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,21 +1,30 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        alphabets = "&ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        alpha = {letter: idx for idx, letter in enumerate(alphabets)}
-        memo = {len(s): 1}
-        
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            # if i == len(s):
-            #     return 1
+        # Bottom up
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = 1
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == '0':
-                return 0
-            count = dfs(i + 1)
+                dp[i] = 0
+                continue
+            dp[i] = dp[i + 1]
             if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
-                count += dfs(i + 2)
-            memo[i] = count
-            return count
+                dp[i] += dp[i + 2]
+        return dp[0]        
+        
+        
+#           Top Down Memo O(n) Time        
+#         memo = {len(s): 1}
+#         def dfs(i):
+#             if i in memo:
+#                 return memo[i]
+#             if s[i] == '0':
+#                 return 0
+#             count = dfs(i + 1)
+#             if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+#                 count += dfs(i + 2)
+#             memo[i] = count
+#             return count
             
-        res = dfs(0)
-        return res
+#         res = dfs(0)
+#         return res
