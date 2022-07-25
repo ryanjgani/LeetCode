2f1d824aefbeecbbd1bc253ffcbaa46a85a58fd5
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-26 00:08:26 +0800

    Time: 729 ms (84.87%), Space: 19.9 MB (8.35%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,14 +1,13 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # Greedy O(n)
-        if sum(gas) < sum(cost):
-            return -1
-
-        total = 0
+        rem = [gas[i] - cost[i] for i in range(len(gas))]
+        print(rem)
+        if sum(gas) < sum(cost): return -1
+        current = 0
         res = 0
-        for i in range(len(gas)):
-            total += gas[i] - cost[i]
-            if total < 0:
-                total = 0
+        for i in range(len(rem)):
+            current += rem[i]
+            if current < 0:
+                current = 0
                 res = i + 1
         return res
