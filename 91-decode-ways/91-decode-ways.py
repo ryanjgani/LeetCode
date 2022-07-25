460f5d74f60b062a01904b65c82597ac9fb86f0e
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-07-25 23:35:17 +0800

    Time: 46 ms (61.81%), Space: 13.8 MB (80.49%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,7 +1,6 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        dp = [0] * (len(s) + 1)
         prev = doubleprev = 1
         for i in range(len(s) - 1, -1, -1):
             if s[i] == '0':
