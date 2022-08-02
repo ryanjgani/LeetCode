2796ede8899c94b765077f2bc4a2660def49ca98
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-02 22:43:32 +0800

    Time: 3427 ms (37.06%), Space: 59.3 MB (7.24%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,16 +1,15 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        arr = []
-        for tri in triplets:
-            arr.append(tri)
-            for j in range(3):
-                if tri[j] > target[j]:
-                    arr.pop()
-                    break
-        rows = len(arr)
-        cols = 3
-        for i in range(rows):
-            for j in range(3):
-                if target[j] and arr[i][j] == target[j]:
-                    target[j] = 0
-        return sum(target) == 0
+        temp = target[:]
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            for i in range(3):
+                if t[i] == target[i]:
+                    temp[i] = 0
+                
+        # for i in range(rows):
+        #     for j in range(3):
+        #         if target[j] and arr[i][j] == target[j]:
+        #             target[j] = 0
+        return sum(temp) == 0
