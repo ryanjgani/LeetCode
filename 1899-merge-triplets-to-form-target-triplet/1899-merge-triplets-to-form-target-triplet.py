d9e45824ae017f85b5a3a605f6becb49c46b5b98
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-02 22:44:06 +0800

    Time: 5004 ms (5.04%), Space: 59.3 MB (33.20%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,5 +1,6 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # O(1) space and O(n) time
         temp = target[:]
         for t in triplets:
             if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
@@ -7,9 +8,4 @@ class Solution:
             for i in range(3):
                 if t[i] == target[i]:
                     temp[i] = 0
-                
-        # for i in range(rows):
-        #     for j in range(3):
-        #         if target[j] and arr[i][j] == target[j]:
-        #             target[j] = 0
         return sum(temp) == 0
