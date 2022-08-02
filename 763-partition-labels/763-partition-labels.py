30a9f029956cc3d58c1bb133e3b4bd3381af00f6
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-02 23:08:50 +0800

    Time: 46 ms (85.67%), Space: 13.9 MB (18.74%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        count = {}
+        for i, c in enumerate(s):
+            count[c] = count.get(c, []) + [i]
+        intervals = []
+        for c in count.values():
+            start, end = c[0], c[-1]
+            intervals.append([c[0], c[-1]])
+        intervals.sort(key = lambda x: x[0])
+        arr = [intervals[0]]
+        
+        for start, end in intervals:
+            s, e = arr[-1][0], arr[-1][1]
+            if start > e:
+                arr.append([start, end])
+            else:
+                arr[-1] = [min(start, s), max(end, e)]
+        res = [(a[1] - a[0] + 1) for a in arr]
+        return res
+        
+        
+        
+        
+        if False:
+            return len(s)
