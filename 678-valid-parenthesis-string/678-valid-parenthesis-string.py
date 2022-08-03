3c13adc9954a0b85a5df562eece96e465a6ecf5c
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-04 00:27:25 +0800

    Time: 105 ms (5.04%), Space: 14.7 MB (8.91%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,31 +1,32 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        left = right = 0
-        for c in s:
-            if c == "*":
-                left += 1
-                right -= 1
-            else:
-                left += 1 if c == "(" else -1
-                right += 1 if c == "(" else -1
-            if left < 0: return False
-            if right < 0: right = 0
-        return right == 0
-        
-        
-        # memo = {}
-        # memo[(len(s), 0)] = True
-        # def dfs(i, count):
-        #     if (i, count) in memo:
-        #         return memo[(i, count)]
-        #     if i >= len(s): return False
-        #     if s[i] == "*":
-        #         temp = dfs(i + 1, count + 1) or dfs(i + 1, count) or dfs(i + 1, count - 1)
+        # left = right = 0
+        # for c in s:
+        #     if c == "*":
+        #         left += 1
+        #         right -= 1
         #     else:
-        #         temp = dfs(i + 1, count + 1) if s[i] == "(" else dfs(i + 1, count - 1)
-        #     memo[(i, count)] = temp
-        #     return temp
-        # return dfs(0, 0)
+        #         left += 1 if c == "(" else -1
+        #         right += 1 if c == "(" else -1
+        #     if left < 0: return False
+        #     if right < 0: right = 0
+        # return right == 0
+        
+        
+        memo = {}
+        # memo[(len(s), 0)] = True
+        def dfs(i, count):
+            if (i, count) in memo:
+                return memo[(i, count)]
+            if i == len(s) or count < 0:
+                return count == 0
+            if s[i] == "*":
+                temp = dfs(i + 1, count + 1) or dfs(i + 1, count) or dfs(i + 1, count - 1)
+            else:
+                temp = dfs(i + 1, count + 1) if s[i] == "(" else dfs(i + 1, count - 1)
+            memo[(i, count)] = temp
+            return temp
+        return dfs(0, 0)
       
         
         
