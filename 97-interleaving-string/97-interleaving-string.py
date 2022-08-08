58aeafedb5371fdf26c619f2046f57c8f3ae47b7
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-08 23:55:30 +0800

    Time: 58 ms (71.15%), Space: 15 MB (29.10%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -4,8 +4,8 @@ class Solution:
         if len(s3) != len(s1) + len(s2):
             return False
         def dfs(i, j, comb):
-            if (i, j, comb) in memo:
-                return memo[(i, j, comb)]
+            if (i, j) in memo:
+                return memo[(i, j)]
             if comb and comb[-1] != s3[len(comb) - 1]:
                 return False
             if i == len(s1) and j == len(s2):
@@ -15,8 +15,8 @@ class Solution:
             if j == len(s2):
                 return comb + s1[i:] == s3
             
-            memo[(i, j, comb)] = dfs(i + 1, j, comb + s1[i]) or dfs(i, j + 1, comb + s2[j])
-            return memo[(i, j, comb)]
+            memo[(i, j)] = dfs(i + 1, j, comb + s1[i]) or dfs(i, j + 1, comb + s2[j])
+            return memo[(i, j)]
         return dfs(0, 0, "")
             
             
