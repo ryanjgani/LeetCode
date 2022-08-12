419f42b51f916b369a520345013bfac075d41d80
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-12 22:48:21 +0800

    Time: 5127 ms (10.85%), Space: 489 MB (5.01%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,35 +1,15 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-#       bottom up
-        target = sum(nums) / 2
         if sum(nums) % 2: return False
+        target = sum(nums) / 2
+        dp = {}
         
-        dp = set()
-        dp.add(0)
-        for n in nums:
-            temp = set()
-            for i in dp:
-                temp.add(i)
-                temp.add(i + n)
-            dp = temp
-        return True if target in dp else False
-        
-        
-        
-        
-#       Top down memoization
-#         dp = {}
-#         def dfs(i, rem):
-#             if (i, rem) in dp:
-#                 return dp[(i, rem)]
+        def dfs(i, comb):
+            if (i, comb) in dp:
+                return dp[(i, comb)]
+            if i == len(nums):
+                return comb == target
+            dp[(i, comb)] = dfs(i + 1, comb + nums[i]) or dfs(i + 1, comb)
+            return dp[(i, comb)]
+        return dfs(0, 0)
             
-#             if rem == 0:
-#                 return True
-#             if rem < 0 or i >= len(nums):
-#                 return False
-
-#             dp[(i, rem)] = dfs(i + 1, rem - nums[i]) or dfs(i + 1, rem)
-#             return dp[(i, rem)]
-        
-#         target = sum(nums) / 2
-#         return False if sum(nums) % 2 else dfs(0, target)
