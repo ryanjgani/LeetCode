0eac5c6dfe126afc13b15a75f9a493e9a81895ce
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-23 19:36:57 +0800

    Time: 40 ms (99.69%), Space: 14 MB (95.83%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -6,12 +6,20 @@ class Solution:
         stack = []
         for i, n in enumerate(nums1):
             hmap[n] = i
-        
-        for i, n in enumerate(nums2):
-            if n in hmap:
-                for j in range(i + 1, len(nums2)):
-                    if n < nums2[j]:
-                        res[hmap[n]] = nums2[j]
-                        break
+            
+        for n in nums2:
+            while stack and n > stack[-1]:
+                res[hmap[stack.pop()]] = n
+                
+            if (not stack or stack[-1] > n) and n in hmap:
+                stack.append(n)
         return res
+        
+        # for i, n in enumerate(nums2):
+        #     if n in hmap:
+        #         for j in range(i + 1, len(nums2)):
+        #             if n < nums2[j]:
+        #                 res[hmap[n]] = nums2[j]
+        #                 break
+        # return res
     
