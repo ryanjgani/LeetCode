3aa81340b085d7e0fed9b58d0678683330e5c9d0
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-25 00:11:14 +0800

    Time: 2517 ms (5.00%), Space: 18.1 MB (34.38%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,22 +1,22 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        wlen = 0
+        length = 0
+        res = len(nums)
         for n in nums:
-            wlen += 1 if n == 1 else 0
-        if not wlen: return 0
-        count = [0] * 2
-        res, l = len(nums), 0
-        for i in range(len(nums)):
-            count[nums[i]] += 1
-            if i >= wlen - 1:
-                res = min(count[0], res)
-                count[nums[l]] -= 1
-                l += 1
-        i = 0
-        while l < len(nums):
-            count[nums[i]] += 1
-            res = min(count[0], res)
-            count[nums[l]] -= 1
-            l += 1
-            i += 1
+            if n: length += 1
+        n = len(nums)
+        nums = nums + nums
+        count = Counter(nums[:length])
+        res = count[0]
+        j = 0
+        for i in range(1, n):
+            if nums[j] == 0:
+                count[0] -= 1
+            if nums[i + length - 1] == 0:
+                count[0] += 1
+            res = min(res, count[0])
+            j += 1
         return res
+        
+        
+        
