76174263c3ec2901c9cdf45e079d91264f674eb2
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-25 22:08:06 +0800

    Time: 1324 ms (19.59%), Space: 36.4 MB (90.21%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -5,39 +5,37 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        def getMid(head):
-            if not head or not head.next:
-                return head
-            slow, fast = head, head.next
-            while fast and fast.next:
-                slow = slow.next
-                fast = fast.next.next
-            return slow
-        
-        def merge(left, right):
-            dummy = ListNode()
+        def merge(h1, h2):
+            dummy = ListNode(-1)
             p = dummy
-            while left and right:
-                if left.val < right.val:
-                    p.next = left
-                    left = left.next
+            while h1 and h2:
+                if h1.val < h2.val:
+                    p.next = h1
+                    h1 = h1.next
+                    p = p.next
                 else:
-                    p.next = right
-                    right = right.next
-                p = p.next
-            p.next = left or right
+                    p.next = h2
+                    h2 = h2.next
+                    p = p.next
+            p.next = h1 or h2
             return dummy.next
+                
         
-        left = head
-        right = getMid(head)
-        tmp = right.next
-        right.next = None
-        right = tmp
+        def mergeSort(head):
+            if not head.next:
+                return head
+            
+            dummy = ListNode(-1, head)
+            slow = fast = dummy
+            while fast and fast.next:
+                fast = fast.next.next
+                slow = slow.next
+            right = mergeSort(slow.next)
+            slow.next = None
+            left = mergeSort(head)
+            return merge(left, right)
+        if not head: return None
+        return mergeSort(head)
+            
         
-        l = self.sortList(left)
-        r = self.sortList(right)
-        return merge(l, r)
         
