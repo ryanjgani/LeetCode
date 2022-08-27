1bf6a42837fe1e5e788c0055f35170f266b33c73
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-08-27 15:56:42 +0800

    Time: 371 ms (80.55%), Space: 16.3 MB (81.67%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,17 +4,27 @@ class Solution:
         d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
         rows, cols = len(grid), len(grid[0])
         
-        def dfs(i, j):
-            if grid[i][j] in ("0", "-1"):
-                return
-            grid[i][j] = '-1'
-            for r, c in d:
-                if 0 <= i + r < rows and 0 <= j + c < cols:
-                    dfs(i + r, j + c)
+        def bfs(row, col):
+            stack = [(row, col)]
+            grid[row][col] = '-1'
+            while stack:
+                i, j = stack.pop()
+                for r, c in d:
+                    if 0 <= i + r < rows and 0 <= j + c < cols and grid[i + r][j + c] == '1':
+                        stack.append((i + r, j + c))
+                        grid[i + r][j + c] = '-1'
+#         def dfs(i, j):
+#             if grid[i][j] in ("0", "-1"):
+#                 return
+#             grid[i][j] = '-1'
+#             for r, c in d:
+#                 if 0 <= i + r < rows and 0 <= j + c < cols:
+#                     dfs(i + r, j + c)
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
-                    dfs(r, c)
+                    # dfs(r, c)
+                    bfs(r, c)
                     res += 1
         return res
             
