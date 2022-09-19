657685b47d3a0f022bdcbc69f16a3d21e2a819ab
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-19 19:30:13 +0800

    Time: 167 ms (33.69%), Space: 14 MB (69.65%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -2,28 +2,25 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         candidates.sort()
-        comb = []
         
-        def dfs(i, total):
+        def dfs(i, comb, total):
             if total == target:
                 res.append(comb[:])
                 return
-            if i >= len(candidates) or total > target:
+            if total > target or i >= len(candidates):
                 return
-            comb.append(candidates[i])
-            dfs(i, total + candidates[i])
-            comb.pop()
-            dfs(i + 1, total)
+            dfs(i, comb + [candidates[i]], total + candidates[i])
+            dfs(i + 1, comb, total)
     
-        # def dfs_long(idx, comb, total):
-        #     if total == target:
-        #         res.append(comb[:])
-        #         return
-        #     if total > target:
-        #         return
-        #     for i in range(idx, len(candidates)):
-        #         if total + candidates[i] > target: break
-        #         dfs(i, comb + [candidates[i]], total + candidates[i])
-        dfs(0, 0)
+        def dfs_long(idx, comb, total):
+            if total == target:
+                res.append(comb[:])
+                return
+            if total > target:
+                return
+            for i in range(idx, len(candidates)):
+                if total + candidates[i] > target: break
+                dfs(i, comb + [candidates[i]], total + candidates[i])
+        dfs(0, [], 0)
         return res
             
