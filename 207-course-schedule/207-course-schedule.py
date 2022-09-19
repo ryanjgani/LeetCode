a03e9bd9449b576584d64d21f89933ce6d9196b8
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-19 16:00:50 +0800

    Time: 244 ms (14.68%), Space: 17.1 MB (40.59%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # [a, b] must take class b first then a
+        hmap = {i: [] for i in range(numCourses)}
+        visit = set()
+        
+        for a, b in prerequisites:
+            hmap[a].append(b)
+        
+        def dfs(n):
+            if n in visit:
+                return False
+            if hmap[n] == []:
+                return True
+            visit.add(n)
+            for prereq in hmap[n]:
+                if not dfs(prereq):
+                    return False
+            visit.remove(n)
+            hmap[n] = []
+            return True
+        for n in range(numCourses):
+            if not dfs(n):
+                return False
+        return True
+        
