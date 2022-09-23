d263e7820eb2a4fcc95aacb91fbd8f5e1d89cc04
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-23 19:05:46 +0800

    Time: 9832 ms (5.01%), Space: 14.1 MB (97.54%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,11 +1,34 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # bottom up dp
         LIS = [1] * len(nums)
         for i in range(len(nums) - 1, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[j] > nums[i]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         return max(LIS)
+        dp = [1] * len(nums)
+        for i in range(len(nums) - 2, -1 , -1):
+            j = i + 1
+            while j < len(nums):
+                if nums[i] <nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+                j += 1
+        return max(dp)
         
-        # Top down memoization ?
+        def dfs(i, prev, count):
+            # if count in dp:
+            #     return dp[count]
+            if i == len(nums):
+                return count
+            temp = 1
+            for j in range(i, len(nums)):
+                if nums[j] > prev:
+                    temp = max(temp, dfs(j, nums[i], count + 1))
+            return temp
+                
+                
+#             if nums[i] > prev:
+#                 temp = max(temp, dfs(i + 1, nums[i], count + 1))
+#             dp[count] = temp
+#             return temp
+        # return dfs(0, float('-inf'), 0)
