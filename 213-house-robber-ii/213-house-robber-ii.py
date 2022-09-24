2326c8129983a81388a81147bb98be44236fe7e5
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-24 12:50:43 +0800

    Time: 69 ms (11.98%), Space: 13.8 MB (98.04%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
         def rob1(nums):
-            if not len(nums): return 0
             res = first = nums[0]
             sec = 0
             for i in range(1, len(nums)):
