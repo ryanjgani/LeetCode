021dda3ecf55e751295cdd137ad6af209596d7db
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-25 19:27:47 +0800

    Time: 209 ms (31.61%), Space: 17.1 MB (25.50%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,40 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # [a, b] must take class b first then a
-        hmap = {i: [] for i in range(numCourses)}
-        visit = set()
-        
+        graph = {i: [] for i in range(numCourses)}
         for a, b in prerequisites:
-            hmap[a].append(b)
-        
+            graph[a].append(b)
+            
+        cycle = set()
         def dfs(n):
-            if n in visit:
-                return False
-            if hmap[n] == []:
-                return True
-            visit.add(n)
-            for prereq in hmap[n]:
-                if not dfs(prereq):
-                    return False
-            visit.remove(n)
-            hmap[n] = []
+            if len(graph[n]) == 0: return True
+            if n in cycle: return False
+            cycle.add(n)
+            for node in graph[n]:
+                if not dfs(node): return False
+            cycle.remove(n)
+            graph[n] = []
             return True
-        for n in range(numCourses):
-            if not dfs(n):
-                return False
-        return True
         
+        for i in range(numCourses):
+            if not dfs(i): return False
+        return True
+    
+#         visit = set()
+#         cycle = set()
+#         def dfs(n):
+#             if n in cycle: 
+#                 return False
+#             visit.add(n)
+#             cycle.add(n)
+#             if n in graph:
+#                 for node in graph[n]:
+#                     if not dfs(node): return False
+#             cycle.remove(n)
+#             return True
+        
+#         for i in range(numCourses):
+#             if i not in visit:
+#                 cycle = set()
+#                 if not dfs(i): return False
+#         return True
+
