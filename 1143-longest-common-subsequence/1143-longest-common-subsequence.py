d529dc217f09a3049bde033bebd19d26ad9161f2
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-25 13:52:49 +0800

    Time: 349 ms (98.70%), Space: 21.9 MB (79.27%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,49 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-#         Top down
-        memo = {}
-        def dfs(i, j):
-            if i == len(text1) or j == len(text2):
-                return 0
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if text1[i] == text2[j]:
-                res = 1 + dfs(i + 1, j + 1)
-            else:
-                res = max(dfs(i + 1, j), dfs(i, j + 1))
-            memo[(i, j)] = res
-            return res
-            
-        return dfs(0, 0)
+        
+        dp = [[0] * (len(text1) + 1) for _ in range(len(text2) + 1)]
+        for r in range(len(text2) - 1, -1, -1):
+            for c in range(len(text1) - 1, -1, -1):
+                if text2[r] == text1[c]:
+                    dp[r][c] = 1 + dp[r + 1][c + 1]
+                else:
+                    dp[r][c] = max(dp[r + 1][c], dp[r][c + 1])
+        return dp[0][0]
         
         
-        
-        
-        # brute force O(2^n)
-#         def dfs(i, j):
-#             if i == len(text1) or j == len(text2):
-#                 return 0
-#             if text1[i] == text2[j]:
-#                 return 1 + dfs(i + 1, j + 1)
-#             else:
-#                 return max(dfs(i + 1, j), dfs(i, j + 1))
-            
-#         return dfs(0, 0)
-        
-        
-        # bottom up
-        # ROWS = len(text1)
-        # COLS = len(text2)
-        # dp = []
-        # for j in range(ROWS + 1):
-        #     row_dp = []
-        #     for i in range(COLS + 1):
-        #         row_dp.append(0)
-        #     dp.append(row_dp)
-        # for i in range(ROWS - 1, -1, -1):
-        #     for j in range(COLS - 1, -1, -1):
-        #         if text1[i] == text2[j]:
-        #             dp[i][j] = 1 + dp[i + 1][j + 1]
-        #         else:
-        #             dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
-        # return dp[0][0]
+        # dp = {}
+        # def dfs(i, j, count):
+        #     if (i, j) in dp: return dp[(i, j)]
+        #     if i == len(text1) or j == len(text2):
+        #         return count
+        #     if text1[i] == text2[j]:
+        #         count += 1
+        #         dp[(i, j)] = max(count, dfs(i + 1, j + 1, count))
+        #     else:
+        #         dp[(i, j)] = max(count, dfs(i + 1, j, count), dfs(i, j + 1, count))
+        #     return dp[(i, j)]
+        # return dfs(0, 0, 0)
