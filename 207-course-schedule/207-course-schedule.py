e890a8d3fb95b6e25079d651ba807cd65a174127
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-25 19:41:46 +0800

    Time: 186 ms (44.29%), Space: 17.4 MB (11.32%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -3,38 +3,31 @@ class Solution:
         graph = {i: [] for i in range(numCourses)}
         for a, b in prerequisites:
             graph[a].append(b)
-            
-        cycle = set()
+        # cycle = set()
+        # def dfs(n):
+        #     if len(graph[n]) == 0: return True
+        #     if n in cycle: return False
+        #     cycle.add(n)
+        #     for node in graph[n]:
+        #         if not dfs(node): return False
+        #     cycle.remove(n)
+        #     graph[n] = []
+        #     return True
+        # for i in range(numCourses):
+        #     if not dfs(i): return False
+        # return True
+    
+        visit = [0] * numCourses
         def dfs(n):
-            if len(graph[n]) == 0: return True
-            if n in cycle: return False
-            cycle.add(n)
+            if visit[n] == 1: return True
+            if visit[n] == -1: return False
+            visit[n] = -1
             for node in graph[n]:
                 if not dfs(node): return False
-            cycle.remove(n)
-            graph[n] = []
+            visit[n] = 1
             return True
         
         for i in range(numCourses):
             if not dfs(i): return False
         return True
-    
-#         visit = set()
-#         cycle = set()
-#         def dfs(n):
-#             if n in cycle: 
-#                 return False
-#             visit.add(n)
-#             cycle.add(n)
-#             if n in graph:
-#                 for node in graph[n]:
-#                     if not dfs(node): return False
-#             cycle.remove(n)
-#             return True
-        
-#         for i in range(numCourses):
-#             if i not in visit:
-#                 cycle = set()
-#                 if not dfs(i): return False
-#         return True
 
