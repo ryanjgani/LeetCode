1e100377cbdb241705f165f7adc9936638eda28a
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-29 18:59:47 +0800

    Time: 155 ms (83.02%), Space: 16 MB (81.81%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,21 +1,37 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         res = 0
-        stack = []
-        maxLeft, maxRight = [], []
-        prev = 0
-        for h in height:
-            maxLeft.append(prev)
-            prev = max(prev, h)
-        prev = 0
-        for i in range(len(height) - 1, -1, -1):
-            maxRight.append(prev)
-            prev = max(prev, height[i])
-        maxRight = maxRight[::-1]
-        
-        for i, h in enumerate(height):
-            count = min(maxLeft[i], maxRight[i]) - h
+        l, r = 0, len(height) - 1
+        maxLeft, maxRight = height[l], height[r]
+        while l < r:
+            if height[l] < height[r]:
+                count = maxLeft - height[l]
+                maxLeft = max(maxLeft, height[l])
+                l += 1
+            else:
+                count = maxRight - height[r]
+                maxRight = max(maxRight, height[r])
+                r -= 1
             if count > 0: res += count
         return res
+                
+        
+        
+        
+        # res = 0
+        # maxLeft, maxRight = [], []
+        # prev = 0
+        # for h in height:
+        #     maxLeft.append(prev)
+        #     prev = max(prev, h)
+        # prev = 0
+        # for i in range(len(height) - 1, -1, -1):
+        #     maxRight.append(prev)
+        #     prev = max(prev, height[i])
+        # maxRight = maxRight[::-1]
+        # for i, h in enumerate(height):
+        #     count = min(maxLeft[i], maxRight[i]) - h
+        #     if count > 0: res += count
+        # return res
             
             
