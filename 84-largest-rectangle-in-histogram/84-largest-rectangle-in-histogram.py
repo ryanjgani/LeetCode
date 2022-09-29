55dd0320663d382f21c50b9a22911c5f22f9b7a1
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-09-29 18:24:35 +0800

    Time: 1643 ms (53.42%), Space: 30.9 MB (25.72%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,42 +1,19 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        # monotonic increasing stack
         stack = []
         res = 0
-        
-        for i, h in enumerate(heights):
-            idx = i
-            while stack and stack[-1][1] > h:
-                top = stack.pop()
-                curArea = (i - top[0]) * top[1]
-                res = max(res, curArea)
-                idx = top[0]
-            stack.append([idx, h])
+        l = 0
+        for r in range(len(heights)):
+            idx = r
+            while stack and stack[-1][0] > heights[r]:
+                h, idx = stack.pop()
+                area = (r - idx) * h
+                res = max(res, area)
+            stack.append((heights[r], idx))
             
         while stack:
-            top = stack.pop()
-            curArea = (len(heights) - top[0]) * top[1]
-            res = max(res, curArea)
+            h, idx = stack.pop()
+            area = (len(heights) - idx) * h
+            res = max(res, area)
         return res
         
-#         l = 0
-#         res = 0
-#         prev = float('inf')
-        
-#         for r, h in enumerate(heights):
-#             # drop
-#             if r > 0 and heights[r] < heights[r - 1]:
-#                 tempL = l
-#                 while heights[l] < heights[r]:
-#                     l += 1
-#                 while l < r:
-#                     curArea = (r - l) * min(heights[l:r])
-#                     res = max(res, curArea)
-#                     l += 1
-#                 l = tempL
-#         r += 1
-#         while l < r:
-#             curArea = (r - l) * min(heights[l:])
-#             res = max(res, curArea)
-#             l += 1
-#         return res
