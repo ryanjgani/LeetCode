eae4f4c6c2690ada08632ab3512fbdd28b7db136
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-10-01 01:39:51 +0800

    Time: 187 ms (51.05%), Space: 14.3 MB (24.52%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,24 +1,24 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        # O(n^2) time
+        # linear time and space
         boxes = [int(n) for n in boxes]
         left = [0] * len(boxes)
         right = [0] * len(boxes)
         ones = boxes[-1]
         for i in range(len(boxes) - 2, -1, -1):
-            right[i] = (ones + right[i + 1]) if ones else 0
+            right[i] = ones + right[i + 1]
             ones += boxes[i]
         ones = boxes[0]
         for i in range(1, len(boxes)):
-            left[i] = (ones + left[i - 1]) if ones else 0
+            left[i] = ones + left[i - 1]
             ones += boxes[i]
         res = [left[i] + right[i] for i in range(len(boxes))]
         return res
         
-        
-        # for i, n in enumerate(boxes):
-        #     if n: ones.append(i)
-        # for i in range(len(boxes)):
-        #     for j in ones:
-        #         res[i] += abs(i - j)
+        # O(n^2) time
+        for i, n in enumerate(boxes):
+            if n: ones.append(i)
+        for i in range(len(boxes)):
+            for j in ones:
+                res[i] += abs(i - j)
         return res
