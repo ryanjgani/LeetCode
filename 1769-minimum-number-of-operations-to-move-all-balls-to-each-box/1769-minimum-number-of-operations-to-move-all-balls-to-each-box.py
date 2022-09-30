521cfbf1fd1d8ae58118c84f406f0f9735a64c40
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-10-01 01:38:26 +0800

    Time: 213 ms (48.19%), Space: 14.3 MB (24.52%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -2,24 +2,16 @@ class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         # O(n^2) time
         boxes = [int(n) for n in boxes]
-        res = [0] * len(boxes)
         left = [0] * len(boxes)
         right = [0] * len(boxes)
-        prev = 0
-        ones = 0
-        for i in range(len(boxes) - 1, -1, -1):
-            if i == len(boxes) - 1: 
-                if boxes[i]: ones += 1
-                continue
+        ones = boxes[-1]
+        for i in range(len(boxes) - 2, -1, -1):
             right[i] = (ones + right[i + 1]) if ones else 0
-            if boxes[i]: ones += 1
-        ones = 0
-        for i in range(len(boxes)):
-            if i == 0: 
-                if boxes[i]: ones += 1
-                continue
+            ones += boxes[i]
+        ones = boxes[0]
+        for i in range(1, len(boxes)):
             left[i] = (ones + left[i - 1]) if ones else 0
-            if boxes[i]: ones += 1
+            ones += boxes[i]
         res = [left[i] + right[i] for i in range(len(boxes))]
         return res
         
