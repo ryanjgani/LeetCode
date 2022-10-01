60e829a91129f4fe6c9f583a5168689652b3d3c2
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-10-01 21:30:45 +0800

    Time: 44 ms (99.32%), Space: 18 MB (46.28%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -6,6 +6,12 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # FOLLOW UP: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+        # Use maxHeap (priority queue) to store the smallest k elements only. 
+        # Constant time lookup and logn time for insertion and deletion.
+        
+        
+        # O(n) time and space iterative
         stack = []
         p = root
         ans = root.val
@@ -21,10 +27,7 @@ class Solution:
             p = p.left
         return ans
         
-        
-        
-        
-        # O(n) time and space
+        # O(n) time and space recursive
         res = []
         def inorder(p):
             if not p: return
