e07eb4867353dfa5431782fc56d10df16e5afadd
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-10-02 01:22:46 +0800

    Time: 90 ms (84.68%), Space: 13.9 MB (95.30%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -5,16 +5,13 @@ class Solution:
         target = {c: 0 for c in alphabets}
         for c in s1:
             target[c] += 1
-        n = len(s1)
-        l = 0
+        
+        l, n = 0, len(s1)
         for r in range(len(s2)):
             hmap[s2[r]] += 1
             if r >= n - 1:
                 ans = True
                 if hmap == target: return True
-                # for key in target:
-                #     if target[key] != hmap[key]:
-                #         ans = False
                 hmap[s2[l]] -= 1
                 l += 1
         return False
