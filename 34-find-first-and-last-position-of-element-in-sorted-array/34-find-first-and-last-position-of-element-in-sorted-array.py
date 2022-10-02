c74f17b98b5c662dddf2c8a5a9492cca5896b767
Ryan <79347718+ryanjgani@users.noreply.github.com>
2022-10-02 15:07:36 +0800

    Time: 223 ms (5.31%), Space: 15.4 MB (49.30%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,30 +2,24 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         l, r = 0, len(nums) - 1
         res = [-1, -1]
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
-            if nums[mid] == target:
-                p = mid
-                while l < p:
-                    m = l + (p - l) // 2
-                    if nums[m] < target:
-                        l = m + 1
-                    else: p = m
-                res[0] = p
-                p = mid
-                while p < r:
-                    add = 1 if (r - p) % 2 else 0
-                    m = add + p + (r - p) // 2
-                    if nums[m] > target:
-                        r = m - 1
-                    else: p = m
-                res[1] = r
-                break
-        return res
+        
+        
+        def helper(pos):
+            l, r = 0, len(nums) - 1
+            i = -1
+            while l <= r:
+                mid = l + (r - l) // 2
+                if nums[mid] < target: l = mid + 1
+                elif nums[mid] > target: r = mid - 1
+                else:
+                    i = mid
+                    if pos == "start":
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+            return i
+        return [helper('start'), helper('end')]
+            
         
         
         
